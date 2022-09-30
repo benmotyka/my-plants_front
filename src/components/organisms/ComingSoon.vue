2d45174d950d5aa9597b4b9f05b7e498a4245afb
ben <benmotykax@gmail.com>
2022-09-30 09:15:40 +0200

    added basic coming soon layout

--- src/components/organisms/ComingSoon.vue
+++ src/components/organisms/ComingSoon.vue
@@ -3,8 +3,8 @@
         class="flex w-full items-center justify-center bg-center bg-cover background-image min-h-[800px] relative lg:mt-20">
         <div class="absolute -bottom-10 flex flex-col-reverse lg:flex-row items-center justify-center px-3 sm:px-10">
             <img src="/coming-soon/watch.png" class="w-[400px] lg:w-[700px]" data-aos="fade-in" />
-            <main class="flex flex-col gap-y-4 pb-14 text-center max-w-[500px] lg:-ml-[150px] ml-0 " data-aos="fade-in">
-                <h4 class="text-4xl text-slate-500 ">Coming soon to Apple Watch</h4>
+            <main class="flex flex-col gap-y-2 pb-14 text-center max-w-[500px] lg:-ml-[150px] ml-0 " data-aos="fade-in">
+                <h2 class="text-4xl text-slate-500 ">Coming soon to Apple Watch</h2>
                 <h5 class="text-slate-500 text-lg font-normal">Phasellus eget nisi eget lectus fringilla vehicula</h5>
                 <p class="mt-4 text-slate-500 text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
                     iaculis, lacus sed viverra interdum,
