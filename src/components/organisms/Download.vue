90fe9e1735efe81f172190949da4c75d8e132bf2
ben <benmotykax@gmail.com>
2022-10-01 11:39:11 +0200

    changed dowlnoad buttons

--- src/components/organisms/Download.vue
+++ src/components/organisms/Download.vue
@@ -3,19 +3,19 @@ import links from "../../config/links"
 
 </script>
 <template>
-    <section id="download" class="flex w-full min-h-[800px] grid grid-cols-2">
+    <section id="download" class="flex w-full min-h-[800px] grid sm:grid-cols-2">
         <main class="flex flex-col items-center justify-center gap-y-2 px-3 sm:px-10 text-center">
             <h2 class="text-4xl text-slate-500">Download now</h2>
             <h5 class="text-slate-500 text-lg font-normal">Short description</h5>
-            <p class="mt-4 text-slate-500 text-sm ">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            <p class="mt-4 text-slate-500 text-sm sm:max-w-md px-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
                 iaculis, lacus sed viverra interdum,
                 nulla mi interdum eros, ac hendrerit urna lectus sit amet massa. Phasellus eget nisi</p>
-            <div class="flex gap-4">
+            <div class="flex w-full gap-8 flex-wrap items-center justify-center py-10">
                 <a :href="links.googlePlay">
-                    <img class="h-40" src="/download/google-play.svg" />
+                    <img class="h-[50px]" src="/download/google-play.png" />
                 </a>
                 <a :href="links.appStore">
-                    <img class="h-40" src="/download/app-store.svg" />
+                    <img class="h-[50px]" src="/download/app-store.png" />
                 </a>
             </div>
         </main>
