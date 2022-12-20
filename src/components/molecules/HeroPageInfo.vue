488e17a111c06898af286683d588cda8bacdaf49
ben <benmotykax@gmail.com>
2022-12-20 22:47:42 +0100

    added i18n config from files

--- src/components/molecules/HeroPageInfo.vue
+++ src/components/molecules/HeroPageInfo.vue
@@ -1,6 +1,6 @@
 <template>
     <img src="/icon.png" class="w-16 h-16" />
-    <h1 class="text-white text-4xl">My plants</h1>
+    <h1 class="text-white text-4xl">{{ $t('applicationName') }}</h1>
     <h5 class="text-white text-xl text-center">
         Free and open source mobile app to track watering of your plants
     </h5>
--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,8 +1,14 @@
 import { createI18n } from "vue-i18n";
+import en from "../../public/locales/en.json";
+import pl from "../../public/locales/pl.json";
 
 const i18n = createI18n({
   //   legacy: false, // for composition api
-  fallbackLocale: "en",
+  fallbackLocale: "pl",
+  messages: {
+    en,
+    pl
+  },
 });
 
 export default i18n;
